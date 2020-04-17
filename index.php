25da89f8ea4c8c9b17236275597fee2c1e75d240
ale777opp <54070882+ale777opp@users.noreply.github.com>
2020-04-17 20:48:00 +0300

    адаптивный дизайн первой страницы

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php session_start();
 $title='Сайт с back-end';
 include 'site_components/head.php';
-?> 
+?>
 
-<div class="container-fluid">
-	    <div class="row" style="margin-top:55px;"> 
-	        <div class="col-md text-center">
+<div class="container-fluid" style="height: calc(100vh - 200px)">
+	    <div class="row" style="margin:7% auto;align-items:center;">
+	        <div class="col col-xl-5 text-center">
       	    <img src='site_components/image/IMG-20190801_300.jpg' class="photo_main" alt='Cаня'><!-- <img src="..." class="img-fluid" alt="Адаптивные изображения"> -->
             </div>
-		    <div class="col-md text-center">
+		    <div class="col col-xl-7 text-center">
             <p> Привет!<br>Меня зовут Александр.<br> Это мой сайт-портфолио для поиска работы. Он демонстрирует, что я могу сделать в области фотографии, Photoshop-а, дизайна, back-end сайтов.
             Буду благодарен всем, кто зарегистрируется и оставит конструктивные замечания.
-                Регистрация на сайте и личный кабинет –элементы back-end сайта. </p>
+                Регистрация на сайте и личный кабинет –элементы back-end сайта.</p>
             </div>
-		</div>	
-</div>	
+		</div>
+</div>
 <footer class="fixed-bottom" style="background-color: #dfdfdf; height: 50px; font-size: 1em; font-family: sans-serif; font-style:italic"> <!-- page-footer font-small blue  -->
  <div class="footer-copyright text-center py-3">© Copyright Шувалов Александр, <?php echo date("F Y");?> </div>
 </footer>
-    
-    
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
